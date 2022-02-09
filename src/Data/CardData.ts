282786f495bcd0dc0ca7457f33abc4b23ed725fa
Racheal-spec <folorunshotosin334@gmail.com>
2022-02-09 14:42:01 +0000

    project structure

---
+++ src/Data/CardData.ts
@@ -0,0 +1,95 @@
+import facebook from '../images/icon-facebook.svg';
+import instagram from '../images/icon-instagram.svg';
+import twitter from '../images/icon-twitter.svg';
+import youtube from '../images/icon-youtube.svg';
+
+export const CardData = [
+    {
+        id: 1,
+        icon: facebook,
+        profilename: '@nathanf',
+        followers: 1987,
+        today: 12,
+    },
+    {
+        id: 2,
+        icon: twitter,
+        profilename: '@nathanf',
+        followers: '1044',
+        today: '99',
+    },
+    {
+        id: 3,
+        icon: instagram,
+        profilename: '@realnathanf',
+        followers: "11k",
+        today: "1099",
+    },
+    {
+        id: 4,
+        icon: youtube,
+        profilename: '@Nathan F',
+        followers: '8239',
+        today: '144',
+    },
+]
+
+export const overviewData = [
+    {
+        id: 12,
+        title: "Page Views",
+        icon: facebook,
+        counts: 87,
+        percent: "3%"
+    },
+    {
+        id: 15,
+        title: "Likes",
+        icon: facebook,
+        counts: '52',
+        percent: "2%"
+    },
+    {
+        id: 22,
+        title: "Likes",
+        icon: instagram,
+        counts: '5462',
+        percent: "2257%"
+    },
+    {
+        id: 29,
+        title: "Profile Views",
+        icon: facebook,
+        counts: "52k",
+        percent: "1375%"
+    },
+    {
+        id: 122,
+        title: "Retweets",
+        icon: twitter,
+        counts: "117",
+        percent: "303%"
+    },
+    {
+        id: 452,
+        title: "Likes",
+        icon: twitter,
+        counts: "507",
+        percent: "553%"
+    },
+    {
+        id: 543,
+        title: "Likes",
+        icon: youtube,
+        counts: "107",
+        percent: "19%"
+    },
+    {
+        id: 23,
+        title: "Total Views",
+        icon: youtube,
+        counts: 1407,
+        percent: "12%"
+    },
+
+]
